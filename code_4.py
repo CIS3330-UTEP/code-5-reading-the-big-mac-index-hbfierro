ee786341f251ce3ce443a9292c1b32f992f67980
hbfierro <hbfierro@outlook.com>
2025-03-02 22:14:55 -0700

    code5

--- code_4.py
+++ code_4.py
@@ -1,18 +1,32 @@
-import csv
+import csv 
 import pandas as pd
 big_mac_file = './big-mac-full-index.csv'
+df=big_mac_file
 
-def get_big_mac_price_by_year(year,country_code):
-    pass # Remove this line and code your function
+
+def get_big_mac_price_by_year(year,country_code): 
+    date_find = round(df.loc[(df['year'] == year) & (df['iso_a3'].str.upper() == country_code), 'dollar_price'].mean(), 2)
+    return date_find
 
 def get_big_mac_price_by_country(country_code):
-    pass # Remove this line and code your function
+    dollar_price = round([df['iso_a3'].str.lower() == country_code, 'dollar_price'].mean(), 2)
+    return dollar_price
 
 def get_the_cheapest_big_mac_price_by_year(year):
-    pass # Remove this line and code your function
+    filtered_df = df[df['year'] == year].nsmallest(1, 'dollar_price')
+    row = filtered_df.iloc[0]
+    return f"{row['name']}({row['iso_a3']}): ${round(row['dollar_price'], 2)}"
 
 def get_the_most_expensive_big_mac_price_by_year(year):
-    pass # Remove this line and code your function
+    row = df.loc[df['year'] == year].nlargest(1, 'dollar_price').iloc[0]
+    return f"{row['name']}({row['iso_a3']}): ${round(row['dollar_price'], 2)}"
 
 if __name__ == "__main__":
-    pass # Remove this line and code your user interface
+    df= pd.read_csv('./big-mac-full-index.csv')
+    year = int(input("Enter year: ").strip())
+    country_code = input("Enter the country code (lowercase): ").strip().lower()
+    print(get_big_mac_price_by_year(year, country_code))
+    print(get_big_mac_price_by_country(country_code))
+    print(get_the_cheapest_big_mac_price_by_year(year))
+    print(get_the_most_expensive_big_mac_price_by_year(year))
+          
